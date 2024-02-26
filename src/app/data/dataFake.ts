85b403869961044ce1f10b010eda797c10faca95
Leonardo Lindoso <leolindoso_2008@hotmail.com>
2024-02-26 00:40:31 -0300

    Finalizando o Blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "saiu novo filme",
+        "description": "ola",
+        "photoCover": "https://th.bing.com/th/id/R.d58acbdf0655493f441f45ab3a8d5bc1?rik=xzAevVRrnGPvjQ&pid=ImgRaw&r=0" 
+    },
+    {
+        "id": "2",
+        "title": "saiu novo filme",
+        "description": "ola",
+        "photoCover": ""
+    } 
+
+]
